6bd427459f1d1e721b2f943e06f520765d15f393
Wojciech Kocjan <wojciech@kocjan.net>
2020-05-05 14:47:40 +0200

    Fix design bug when skill name is long

--- gatsby-theme-intro/src/components/skills/skill.js
+++ gatsby-theme-intro/src/components/skills/skill.js
@@ -6,7 +6,7 @@ const Skill = ({ skill, i }) => (
   <li
     className={`relative flex justify-center items-center rounded-full border-2 border-skill-${i}`}
   >
-    <span className="absolute font-header font-semibold text-front text-sm md:text-base">
+    <span className="absolute font-header font-semibold text-front text-sm md:text-base px-2 text-center">
       {skill}
     </span>
     <SkillSvg className={`w-full text-skill-${i} opacity-75 m-2`} />
