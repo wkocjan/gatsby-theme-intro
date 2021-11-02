8c77164eade30ef9a509d6af7170257d3f4fbdb6
reduards <rasmuseduards@gmail.com>
2021-11-02 10:46:00 +0100

    Commit made from VS Code Azure Static Web Apps

---
+++ my-site/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    description: "Personal page of Rasmus Eduards",
+    locale: "en",
+    title: "Rasmus Eduards",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        basePath: "/",
+        contentPath: "content/",
+        showThemeLogo: true,
+        theme: "dark-green",
+      },
+    },
+  ],
+}
