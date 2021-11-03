2a33e28080dca10e96e59e3e10a634a56bbcd661
reduards <rasmuseduards@gmail.com>
2021-11-03 09:17:34 +0100

    changed theme

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
         basePath: "/",
         contentPath: "content/",
         showThemeLogo: true,
-        theme: "classic",
+        theme: "dark-green",
       },
     },
   ],
