1cdd2eceaeda720459ef83b36e56333a8f57a18c
reduards <rasmuseduards@gmail.com>
2021-11-07 16:12:09 +0100

    added description metadata

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    description: "Personal page of John Doe",
+    description: "Personal page of Rasmus Eduards",
     locale: "en",
-    title: "John Doe",
+    title: "Rasmus Eduards",
   },
   plugins: [
     {
