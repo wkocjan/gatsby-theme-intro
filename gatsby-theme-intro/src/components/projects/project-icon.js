d148b5e16ac09ec84a229410cd2d803af5b1f1df
Daniel Chick <daniel.chick@hotmail.com>
2022-03-26 20:46:23 -0500

    Refactor project icons to be extensible like social icons

--- gatsby-theme-intro/src/components/projects/project-icon.js
+++ gatsby-theme-intro/src/components/projects/project-icon.js
@@ -2,13 +2,26 @@ import React from "react"
 import { FaCompass, FaGithub } from "react-icons/fa"
 import { ProjectType } from "../../types"
 
-const ProjectIcon = ({ icon }) => (
+const ProjectIcon = ({ icon }) => {
+  const icons = {
+    github: FaGithub,
+    website: FaCompass,
+  }
+
+  const Icon = icons[icon]
+
+  return Icon ? ChosenIcon(Icon) : WebsiteIcon
+}
+
+const ChosenIcon = ( Icon ) => (
   <span className="absolute right-0 bottom-0 mb-5 mr-5 text-back">
-    {icon === "github" ? (
-      <FaGithub className="w-6 h-6" />
-    ) : (
-      <FaCompass className="w-6 h-6" />
-    )}
+    <Icon className="w-6 h-6" />
+  </span>
+)
+
+const WebsiteIcon = () => (
+  <span className="absolute right-0 bottom-0 mb-5 mr-5 text-back">
+    <FaCompass className="w-6 h-6" />
   </span>
 )
 
